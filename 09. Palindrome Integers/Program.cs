b9915443c966d4fbe7aae1d1041000dcc2eb72df
Aleksandrino04 <106624793+Aleksandrino04@users.noreply.github.com>
2022-06-15 17:04:21 +0300

    Add files via upload

---
+++ 09. Palindrome Integers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09_PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = Console.ReadLine();
+            bool isPalindromeIntegers = false;
+            while (number != "END")
+            {
+                Console.WriteLine(PrintPalidrome(number, isPalindromeIntegers).ToString().ToLower());
+                number = Console.ReadLine();       
+            }                                      
+        }
+        private static bool PrintPalidrome(string number, bool isPalindromeItegers)
+        {
+            var reverseNumber = "";
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                char symbol = number[i];
+                reverseNumber += symbol;
+            }
+            if (reverseNumber == number)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
